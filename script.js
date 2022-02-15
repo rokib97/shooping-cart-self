8ce25849f086292909b15edc3dbc07a70e54d949
rokib97 <hasanrokib1997@gmail.com>
2022-02-15 15:33:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function productUpdate(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + "-number");
+  let productNumber = productInput.value;
+  if (isIncreasing) {
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productInput.value > 0) {
+    productNumber = parseInt(productNumber) - 1;
+  }
+  productInput.value = productNumber;
+
+  const productPrice = document.getElementById(product + "-price");
+  productPrice.innerText = productNumber * price;
+  calculateTotal();
+}
+
+function getInputValue(product) {
+  const productInput = document.getElementById(product + "-number");
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+
+function calculateTotal() {
+  const phoneTotal = getInputValue("phone") * 1240;
+  const caseTotal = getInputValue("case") * 59;
+  const subTotal = phoneTotal + caseTotal;
+  const tax = subTotal / 10;
+  const totalPrice = subTotal + tax;
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-amount").innerText = tax;
+  document.getElementById("total-price").innerText = totalPrice;
+}
+document
+  .getElementById("phone-plus-btn")
+  .addEventListener("click", function () {
+    productUpdate("phone", 1240, true);
+  });
+document
+  .getElementById("phone-minus-btn")
+  .addEventListener("click", function () {
+    productUpdate("phone", 1240, false);
+  });
+document.getElementById("case-plus-btn").addEventListener("click", function () {
+  productUpdate("case", 59, true);
+});
+document
+  .getElementById("case-minus-btn")
+  .addEventListener("click", function () {
+    productUpdate("case", 59, false);
+  });
